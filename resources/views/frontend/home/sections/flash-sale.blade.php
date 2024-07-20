526ea2dcf1e6d5d4fdb8c14f33b00cfe7eba860f
tuonglvph24343 <tuonglvph24343@fpt.edu.vn>
2024-07-20 15:32:32 +0700

    fix

--- resources/views/frontend/home/sections/flash-sale.blade.php
+++ resources/views/frontend/home/sections/flash-sale.blade.php
@@ -53,9 +53,9 @@
                             </p>
                             <a class="wsus__pro_name" href="{{route('product-detail',$product->slug)}}">{{ $product->name }}</a>
                             @if (checkDiscount($product))
-                                <p class="wsus__price">{{$settings->currency_icon}}{{ $product->offer_price }}<del>{{$settings->currency_icon}}{{ $product->price }}</del></p>
+                                <p class="wsus__price">{{ $product->offer_price }} {{$settings->currency_icon}}<del>{{ $product->price }} {{$settings->currency_icon}}</del></p>
                             @else
-                                <p class="wsus__price">{{$settings->currency_icon}}{{ $product->price }}</p>
+                                <p class="wsus__price">{{ $product->price }} {{$settings->currency_icon}}</p>
                             @endif
 
                             <a class="add_cart" href="#">add to cart</a>
